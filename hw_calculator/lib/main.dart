9b2342f6ce7939120c27300938a8079c6d847cc3
Azamat Chambulov <chambulov2019@gmail.com>
2025-03-19 00:05:27 +0600

    homeworks 1-5

---
+++ hw_calculator/lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'calculator.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Калькулятор',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.grey,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: CalculatorPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  String _expression = '';
+  String _result = '';
+
+  void _onButtonPressed(String value) {
+    setState(() {
+      if (value == 'C') {
+        _expression = '';
+        _result = '';
+      } else if (value == 'DEL') {
+        if (_expression.isNotEmpty) {
+          _expression = _expression.substring(0, _expression.length - 1);
+        }
+      } else if (value == '=') {
+        try {
+          _result = evaluateExpression(_expression);
+        } catch (e) {
+          _result = 'Error';
+        }
+      } else {
+        _expression += value;
+      }
+    });
+  }
+
+  Widget _buildButton(String text) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(padding: EdgeInsets.all(20)),
+          onPressed: () => _onButtonPressed(text),
+          child: Text(text, style: TextStyle(fontSize: 18)),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Калькулятор'), centerTitle: true),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(12),
+            alignment: Alignment.centerRight,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  _expression,
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  _result,
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(color: Colors.white),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    _buildButton('C'),
+                    _buildButton('DEL'),
+                    _buildButton('('),
+                    _buildButton(')'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    _buildButton('7'),
+                    _buildButton('8'),
+                    _buildButton('9'),
+                    _buildButton('/'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    _buildButton('4'),
+                    _buildButton('5'),
+                    _buildButton('6'),
+                    _buildButton('*'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    _buildButton('1'),
+                    _buildButton('2'),
+                    _buildButton('3'),
+                    _buildButton('-'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    _buildButton('0'),
+                    _buildButton('.'),
+                    _buildButton('='),
+                    _buildButton('+'),
+                  ],
+                ),
+
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Wrap(
+                      spacing: 4,
+                      runSpacing: 4,
+                      children: [
+                        _buildButton('sin('),
+                        _buildButton('cos('),
+                        _buildButton('tan('),
+                        _buildButton('cot('),
+                        _buildButton('**'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
