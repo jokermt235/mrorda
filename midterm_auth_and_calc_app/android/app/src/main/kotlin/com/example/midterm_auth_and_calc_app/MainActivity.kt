53505183f9c3b078e3c9bcdc923d281792f0cede
rasim010101 <qvvzxcvqvv@mail.ru>
2025-03-19 16:06:23 +0600

    Added calculator

---
+++ midterm_auth_and_calc_app/android/app/src/main/kotlin/com/example/midterm_auth_and_calc_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.midterm_auth_and_calc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ midterm_auth_and_calc_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+import 'user_service.dart';
+
+void main() {
+  UserService().initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
