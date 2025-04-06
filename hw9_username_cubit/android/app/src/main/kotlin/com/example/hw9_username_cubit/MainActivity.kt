1b0152ad07530ec7ac2ecbb65344f16417ba82c9
eldanaomushova <eldana956@gmail.com>
2025-04-06 17:37:25 +0600

    username hw

---
+++ hw9_username_cubit/android/app/src/main/kotlin/com/example/hw9_username_cubit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hw9_username_cubit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ hw9_username_cubit/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'username_cubit.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => UsernameCubit(),
+        child: const UsernamePage(),
+      ),
+    );
+  }
+}
+
+class UsernamePage extends StatelessWidget {
+  const UsernamePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final usernameCubit = context.read<UsernameCubit>();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Username Duplicator')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Enter Username'),
+              onChanged: (value) => usernameCubit.updateUsername(value),
+            ),
+            const SizedBox(height: 20),
+            BlocBuilder<UsernameCubit, String>(
+              builder: (context, duplicatedUsername) {
+                return Text(
+                  duplicatedUsername,
+                  style: const TextStyle(fontSize: 24),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
