753857b7d985f4d852eba6901c2249d2b97c9bd8
Aigerim Nuralieva <75246512+aiger555@users.noreply.github.com>
2025-04-18 21:04:13 +0600

    Delete lib2 - HWs directory

--- lib2 - HWs/static.dart
+++
@@ -1,9 +0,0 @@
-class Example {
-  static void doNothing() {
-    print("This method does nothing.");
-  }
-}
-
-void main() {
-  Example.doNothing();
-}
--- lib2 - HWs/sum.dart
+++
@@ -1,6 +0,0 @@
-void main() {
-  int a = 10;
-  int b = 20;
-  int sum = a + b;
-  print("sum: $sum");
-}
