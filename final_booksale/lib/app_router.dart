a7f16756b4461b775dc52199964f06c3a3110ec2
tuerto-puerto <torotura@icloud.com>
2025-04-29 01:17:51 +0600

    Book Sale project

---
+++ final_booksale/lib/app_router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../views/home_page.dart';
+import '../views/book_view_page.dart';
+import '../views/cart_view_page.dart';
+
+final GoRouter router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+      routes: [
+        GoRoute(
+          path: 'book/:id',
+          builder:
+              (context, state) =>
+                  BookViewPage(bookId: state.pathParameters['id']!),
+        ),
+        GoRoute(
+          path: 'cart',
+          builder: (context, state) => const CartViewPage(),
+        ),
+      ],
+    ),
+  ],
+);
